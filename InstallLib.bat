9d6b3b84c25491c368d59c32e56ef05e567decc5
Tb114 <tb1145@outlook.com>
2025-07-01 22:57:17 +0800

    Version 0.06.5 - 增加一键安装依赖的脚本

---
+++ InstallLib.bat
@@ -0,0 +1,1 @@
+pip install dotenv Pillow pytz keyboard -i https://pypi.tuna.tsinghua.edu.cn/simple
---
+++ InstallLib.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pip install dotenv Pillow pytz keyboard -i https://pypi.tuna.tsinghua.edu.cn/simple
